10c2d6c02533c38b3aff4c1f66ae3e7bcba691c7
Cristian Prigoana <cristian.prigoana@yahoo.com>
2016-03-29 23:26:35 +0200

    Add a placeholder for the degree centrality chart

--- resources/d3/chartsDisplay.js
+++ resources/d3/chartsDisplay.js
@@ -2,17 +2,35 @@
  * Created by cristiprg on 29-3-16.
  */
 
-function displayTimeDistributionChart(){
-    document.getElementById("chartsDisplay").style.height = "200px";
+var timeDistributionChart;
+var avgDegreeCentralityChart;
 
-    var chart = c3.generate({
-        bindto: '#chartsDisplay',
+function displayTimeDistributionChart(){
+    document.getElementById("timeDistributionChart").style.height = "200px";
+
+    timeDistributionChart = c3.generate({
+        bindto: '#timeDistributionChart',
         data: {
             columns: [
                 ['data1', 30, 200, 100, 400, 150, 250],
                 ['data2', 50, 20, 10, 40, 15, 25]
             ],
             type: 'bar'
+        }
+    });
+
+}
+
+function displayAvgDegreeCentralityChart(){
+    document.getElementById("avgDegreeCentralityChart").style.height = "200px";
+
+    avgDegreeCentralityChart = c3.generate({
+        bindto: '#avgDegreeCentralityChart',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ]
         }
     });
 }
