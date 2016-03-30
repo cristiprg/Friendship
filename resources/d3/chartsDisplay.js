3c708cf9e9ae15d165b909616f7e4457fe532238
Cristian Prigoana <cristian.prigoana@yahoo.com>
2016-03-31 00:59:05 +0200

    Display cumulative distribution

--- resources/d3/chartsDisplay.js
+++ resources/d3/chartsDisplay.js
@@ -13,15 +13,13 @@ function displayTimeDistributionChart(){
     $.get('timeDistribution', function(response){
         var chartData = JSON.parse(response);
         chartData.per_hour.unshift('per_hour');
-        chartData.per_month.unshift('per_month');
-        chartData.per_day.unshift('per_day');
 
         console.log(chartData.per_day);
 
         timeDistributionChart = c3.generate({
             bindto: '#timeDistributionChart',
             data: {
-                columns: [chartData.per_hour, chartData.per_month, chartData.per_day],
+                columns: [chartData.per_hour],
                 type: 'bar'
             }
         });
@@ -32,13 +30,23 @@ function displayTimeDistributionChart(){
 function displayAvgDegreeCentralityChart(){
     document.getElementById("avgDegreeCentralityChart").style.height = "200px";
 
-    avgDegreeCentralityChart = c3.generate({
-        bindto: '#avgDegreeCentralityChart',
-        data: {
-            columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
-            ]
-        }
+    $.get('timeDistribution', function(response){
+        var chartData = JSON.parse(response);
+        chartData.cumulative.ticks.unshift('x');
+        chartData.cumulative.values.unshift('friendships');
+        console.log(chartData);
+
+        avgDegreeCentralityChart = c3.generate({
+            bindto: '#avgDegreeCentralityChart',
+            data: {
+                x: 'x',
+                columns: [chartData.cumulative.ticks,chartData.cumulative.values]
+            },
+            axis: {
+                x: {
+                    type: 'timeseries'
+                }
+            }
+        });
     });
 }
